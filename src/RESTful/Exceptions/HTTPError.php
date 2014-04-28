d7a6b017a24f727cfff0cd13a66e9bc1e451d8fe
Richard Serna <Bigpapa1>
2014-04-28 11:38:09 -0700

    HTTP encode URI before passing

--- src/RESTful/Exceptions/HTTPError.php
+++ src/RESTful/Exceptions/HTTPError.php
@@ -21,16 +21,7 @@ class HTTPError extends Base
 
     protected function _objectify($fields)
     {
-        if (empty($fields->errors[0])) {
-            $fields = array(
-                'status'  => "Bad Request",
-                'category_code' => "Bad Request",
-                'description' => "Bad Request",
-                'status_code' => 404,
-            );
-        } else {
-            $fields = $fields->errors[0];
-        };
+        $fields = $fields->errors[0];
         foreach ($fields as $key => $val) {
             $this->$key = $val;
         }
